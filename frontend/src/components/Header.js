bfee953090f890310209c609b3a819eb5e02a482
Raphael Mächler <r_maechler@outlook.com>
2021-07-13 15:13:51 +0200

    welcome component

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -10,7 +10,7 @@ const Header = ({ title }) => {
   return (
     <Navbar style={navbarStyle} variant="light">
       <Container>
-        <Logo style={{ maxWidth: '16rem', maxHeight: '4rem' }} />
+        <Logo alt={title} style={{ maxWidth: '16rem', maxHeight: '4rem' }} />
       </Container>
     </Navbar>
   );
